958f1bbc39352fa16b804f50d90c6c859ec713ab
Dana Prakoso <danaossoftware@gmail.com>
2019-02-25 06:58:26 +0700

    commit

--- php/add-order.php
+++ php/add-order.php
@@ -1,5 +1,8 @@
 <?php
 include 'db.php';
+session_id("kongsicargo");
+session_start();
+$userId = $_SESSION["kongsicargo_user_id"];
 $fromLat = doubleval($_POST["from_lat"]);
 $fromLng = doubleval($_POST["from_lng"]);
 $toLat = doubleval($_POST["to_lat"]);
@@ -22,4 +25,6 @@ $distance = doubleval($_POST["distance"]);
 $price = intval($_POST["price"]);
 $locBenchmarkFrom = doubleval($_POST["loc_benchmark_from"]);
 $locBenchmarkTo = doubleval($_POST["loc_benchmark_to"]);
-$c->query("INSERT INTO orders (id, from_lat, from_lng, to_lat, to_lng, truck_type, sender_name, sender_phone, receiver_name, receiver_phone, paid_by_sender, order_date, item_data, item_type, img_url, driver_note, extra_service, extra_help, voucher_code, distance, price, loc_benchmark_from, loc_benchmark_to) VALUES ('" . uniqid() . "', " . $fromLat . ", " . $fromLng . ", " . $toLat . ", " . $toLng . ", " . $truckType . ", '" . $senderName . "', '" . $senderPhone . "', '" . $receiverName . "', '" . $receiverPhone . "', " . $paidBySender . ", " . $orderDate . ", '" . $itemData . "', " . $itemType . ", '" . $imgURL . "', '" . $driverNote . "', " . $extraService . ", '" . $extraHelp . "', '" . $voucherCode . "', " . $distance . ", " . $price . ", " . $locBenchmarkFrom . ", " . $locBenchmarkTo . ")");
+$orderId = uniqid();
+$c->query("INSERT INTO orders (id, from_lat, from_lng, to_lat, to_lng, truck_type, sender_name, sender_phone, receiver_name, receiver_phone, paid_by_sender, order_date, item_data, item_type, img_url, driver_note, extra_service, extra_help, voucher_code, distance, price, loc_benchmark_from, loc_benchmark_to) VALUES ('" . $orderId . "', " . $fromLat . ", " . $fromLng . ", " . $toLat . ", " . $toLng . ", " . $truckType . ", '" . $senderName . "', '" . $senderPhone . "', '" . $receiverName . "', '" . $receiverPhone . "', " . $paidBySender . ", " . $orderDate . ", '" . $itemData . "', " . $itemType . ", '" . $imgURL . "', '" . $driverNote . "', " . $extraService . ", '" . $extraHelp . "', '" . $voucherCode . "', " . $distance . ", " . $price . ", " . $locBenchmarkFrom . ", " . $locBenchmarkTo . ")");
+$c->query("UPDATE users SET current_order_id='" . $orderId . "' WHERE id='" . $userId . "'");

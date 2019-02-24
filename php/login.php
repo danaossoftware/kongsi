c4f77cfec36c5d361f50602f581fef49ea451623
Dana Prakoso <danaossoftware@gmail.com>
2019-02-24 23:42:51 +0700

    commit

--- php/login.php
+++ php/login.php
@@ -15,6 +15,8 @@ if ($results && $results->num_rows > 0) {
 	}
 	session_start();
 	$_SESSION["kongsicargo_user_id"] = $row["id"];
+	$params = session_get_cookie_params();
+	setcookie(session_name(), $_COOKIE[session_name()], time() + 7*24*3600, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
 	echo 0;
 } else {
 	echo -1;

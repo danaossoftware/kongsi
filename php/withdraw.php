6cfada3acf218ebf5d6352d4d4dd854cb6075d8e
Dana Prakoso <danaossoftware@gmail.com>
2019-02-26 06:20:33 +0700

    commit

--- php/withdraw.php
+++ php/withdraw.php
@@ -4,5 +4,5 @@ session_id("kongsicargo");
 session_start();
 $userId = $_SESSION["kongsicargo_user_id"];
 $amount = intval($_POST["amount"]);
-$date = intval($_POST["date"]);
-$c->query("INSERT INTO pending_withdraw (id, user_id, amount, date, status) VALUES ('" . uniqid() . "', '" . $userId . "', " . $amount . ", " . round(microtime(true)*1000) . ", 'pending')");
+$date = round(microtime(true)*1000);
+$c->query("INSERT INTO pending_withdraw (id, user_id, amount, date, status) VALUES ('" . uniqid() . "', '" . $userId . "', " . $amount . ", " . $date . ", 'pending')");

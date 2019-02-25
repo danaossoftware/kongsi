f77aa86712d4dd648c264598f86250763dd6716d
Dana Prakoso <danaossoftware@gmail.com>
2019-02-26 04:35:38 +0700

    commit

--- php/upload-proof.php
+++ php/upload-proof.php
@@ -4,7 +4,7 @@ session_id("kongsicargo");
 session_start();
 $userId = $_SESSION["kongsicargo_user_id"];
 $fileName = $_POST["file_name"];
-$fileURL = "http://192.168.43.139/kongsi/userdata/imgs/" . $fileName;
+$fileURL = "http://103.103.175.239/kongsi/userdata/imgs/" . $fileName;
 move_uploaded_file($_FILES["file"]["tmp_name"], "../userdata/imgs/" . $fileName);
 $results = $c->query("SELECT * FROM pending_top_up WHERE user_id='" . $userId . "'");
 if ($results && $results->num_rows > 0) {

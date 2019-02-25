53bedf9070c0a6afb3f575dfdba43edeaa5eca7c
Dana Prakoso <danaossoftware@gmail.com>
2019-02-26 06:52:53 +0700

    commit

--- php/find-user.php
+++ php/find-user.php
@@ -1,4 +1,11 @@
 <?php
 include 'db.php';
 $keyword = $_POST["keyword"];
-$c->query("SELECT * FROM users WHERE name LIKE '%" . $keyword . "%' OR email LIKE '%" . $keyword . "%'");
+$results = $c->query("SELECT * FROM users WHERE name LIKE '%" . $keyword . "%' OR email LIKE '%" . $keyword . "%'");
+$users = [];
+if ($results && $results->num_rows > 0) {
+	while ($row = $results->fetch_assoc()) {
+		array_push($users, $row);
+	}
+}
+echo json_encode($users);

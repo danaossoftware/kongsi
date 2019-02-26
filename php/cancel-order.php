81162f7ce480f38e6760b8e385a9ce0237e10e51
Dana Prakoso <danaossoftware@gmail.com>
2019-02-26 13:18:01 +0700

    commit

--- php/cancel-order.php
+++ php/cancel-order.php
@@ -3,4 +3,12 @@ include 'db.php';
 session_id("kongsicargo");
 session_start();
 $userId = $_SESSION["kongsicargo_user_id"];
+$results = $c->query("SELECT * FROM users WHERE id='" . $userId . "'");
+if ($results && $results->fetch_assoc()) {
+	$row = $results->fetch_assoc();
+	$currentOrderID = $row["current_order_id"];
+	if ($currentOrderID != "") {
+		$c->query("DELETE FROM orders WHERE id='" . $currentOrderID . "'");
+	}
+}
 $c->query("UPDATE users SET current_order_id='' WHERE id='" . $userId . "'");

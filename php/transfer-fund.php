d60f5f49b6d7c141dec8a45a758b6938a8ff2097
Dana Prakoso <danaossoftware@gmail.com>
2019-02-26 07:10:57 +0700

    commit

--- php/transfer-fund.php
+++ php/transfer-fund.php
@@ -1,6 +1,8 @@
 <?php
 include 'db.php';
-$fromId = $_POST["from"];
+session_id("kongsicargo");
+session_start();
+$fromId = $_SESSION["kongsicargo_user_id"];
 $toId = $_POST["to"];
 $amount = intval($_POST["amount"]);
 $c->query("INSERT INTO fund_transfers (id, from_id, to_id, amount, date, status) VALUES ('" . uniqid() . "', '" . $fromId . "', '" . $toId . "', " . $amount . ", " . round(microtime(true)*1000) . ", 'pending')");

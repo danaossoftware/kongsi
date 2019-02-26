4dbace7a3d06775b5b1e59070339cfb0f07b8f0d
Dana Prakoso <danaossoftware@gmail.com>
2019-02-26 13:39:58 +0700

    commit

--- php/find-nearest-drivers.php
+++ php/find-nearest-drivers.php
@@ -2,7 +2,7 @@
 include 'db.php';
 $lat = doubleval($_GET["lat"]);
 $lng = doubleval($_GET["lng"]);
-$results = $c->query("SELECT latitude, longitude, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM drivers HAVING distance < 25");
+$results = $c->query("SELECT * SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM drivers HAVING distance < 25");
 $users = [];
 if ($results && $results->num_rows > 0) {
 	while ($row = $results->fetch_assoc()) {

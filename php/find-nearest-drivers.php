5ecd4b4dd2fd23776809b57461b0d0922239725b
Dana Prakoso <danaossoftware@gmail.com>
2019-02-27 05:41:13 +0700

    commit

--- php/find-nearest-drivers.php
+++ php/find-nearest-drivers.php
@@ -2,7 +2,8 @@
 include 'db.php';
 $lat = doubleval($_POST["lat"]);
 $lng = doubleval($_POST["lng"]);
-$results = $c->query("SELECT *, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM drivers HAVING distance < 25 ORDER BY distance");
+$vehicle = $_POST["container_name"];
+$results = $c->query("SELECT *, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM drivers WHERE vehicle='" . $vehicle . "' HAVING distance < 25 ORDER BY distance");
 $users = [];
 if ($results && $results->num_rows > 0) {
 	while ($row = $results->fetch_assoc()) {

78f75592aad1c1c8a8eff383328fd2f79f77f8b0
Bob <danaoscompany@gmail.com>
2019-04-22 00:17:38 +0700

    commit

---
+++ php/generate-referral-code.php
@@ -0,0 +1,12 @@
+<?php
+include 'db.php';
+while (true) {
+	$randomCode = uniqid();
+	$randomCode = substr($randomCode, 0, 5);
+	$results = $c->query("SELECT * FROM referral_codes WHERE code='" . $randomCode . "'");
+	if ($results && $results->num_rows > 0) {
+		continue;
+	} else {
+		echo $randomCode;
+	}
+}
